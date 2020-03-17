35c37ccecf4347a68f72e78e60d53c9484692a24
kosmonout <kosmonout@gmail.com>
2020-03-17 22:43:26 +0100

    Update Candle Light

--- projects/FireLightBoat/FireLightNeo/FireLightNeo.ino
+++ projects/FireLightBoat/FireLightNeo/FireLightNeo.ino
@@ -1,26 +1,29 @@
 #include <Adafruit_NeoPixel.h>
 
 #define NUM_PIXELS 4
+#define DELAY_START 50
+#define DELAY_STOP 400
+#define COLOR_START 50
+#define COLOR_STOP 80
 
 Adafruit_NeoPixel pixels(NUM_PIXELS, 2, NEO_GRB | NEO_KHZ800);
 
 void setup() {
   pixels.begin();
-}
-
-void setColor(uint32_t color) {
   for (int i = 0; i < NUM_PIXELS; i++) {
-    pixels.setPixelColor(i, color);
+    pixels.setPixelColor(i, 0);
     pixels.show();
-    delay(50);
   }
 }
 
+void setColor(uint32_t color) {
+     pixels.setPixelColor(0, color);
+     pixels.show();
+}
+
 void loop() {
-  setColor(pixels.Color(100, 0, 0));
-  delay(1000);
-  setColor(pixels.Color(0, 100, 0));
-  delay(1000);
-  setColor(pixels.Color(0, 0, 100));
-  delay(1000);
+  int red = random(COLOR_START, COLOR_STOP);
+  int green = red*0.67;
+  setColor(pixels.Color(red, green, 0));
+  delay(random(DELAY_START, DELAY_STOP));
 }
